169823bbb1c87f5a859ff38a322a1ff016ed8960
Deva Satrya Ramadhan <devasa3887@gmail.com>
2024-03-13 20:49:16 +0700

    first commit

---
+++ js/dasar.js
@@ -0,0 +1,6 @@
+function tampilkanInfo() {
+    var nama = document.getElementById("nama").value;
+    var umur = document.getElementById("umur").value;
+
+    console.log("Nama:", nama, "Umur:", umur);
+  }
